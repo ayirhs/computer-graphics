fb30286512f77fa03b13ec5d65f33c3c537223c2
blueshriya <106502516+blueshriya@users.noreply.github.com>
2023-08-17 13:33:34 +0545

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+#include <conio.h>
+#include <graphics.h>
+#include <dos.h>
+
+void main()
+
+{
+    int gd = DETECT, gm;
+
+    initgraph(&gd, &gm, "C:\\TurboC3\\BGI");
+    line(100, 100, 200, 200);
+    printf("Shriya K");
+    printf("\n");
+    printf("Roll no. 128");
+    getch();
+}
---
+++ 10.c
@@ -0,0 +1,71 @@
+#include<stdio.h> 
+
+#include<conio.h> 
+
+#include<graphics.h> 
+
+#include<dos.h> 
+
+void flood(int, int, int, int); 
+
+
+
+void main() 
+
+{  
+
+int gd,gm=DETECT; 
+
+clrscr(); 
+
+detectgraph(&gd,&gm); 
+
+initgraph(&gd,&gm,"C:\\TurboC3\\BGI");
+printf("Shriya k");
+printf("\n\n");
+
+printf("Roll no: 128");
+ printf("\n\n");
+
+
+rectangle(200,200,250,250); 
+
+flood(220,220,7,0); 
+
+getch();  
+
+} 
+
+
+
+void flood(int x, int y, int fill_col, int old_col) 
+
+{  
+
+if(getpixel(x,y)==old_col) 
+
+{  
+
+delay(10); 
+
+putpixel(x,y,fill_col); 
+
+flood(x+1,y,fill_col,old_col); 
+
+flood(x-1,y,fill_col,old_col); 
+
+flood(x,y+1,fill_col,old_col); 
+
+flood(x,y-1,fill_col,old_col); 
+
+flood(x + 1, y - 1, fill_col, old_col); 
+
+flood(x + 1, y + 1, fill_col, old_col); 
+
+flood(x - 1, y - 1, fill_col, old_col); 
+
+flood(x - 1, y + 1, fill_col, old_col); 
+
+} 
+
+}  
