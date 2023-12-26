d9f82eebf139027a69932a10027b8d12917f5cf5
ayirhs <106502516+ayirhs@users.noreply.github.com>
2023-12-26 19:09:37 +0545

    Add files via upload

--- 26.c
+++ 26.c
@@ -45,3 +45,57 @@ void renderTriangle(Vertex v1, Vertex v2, Vertex v3) {
         v3 = temp;
     }
     if (v1.y > v2.y) {
+        Vertex temp = v1;
+        v1 = v2;
+        v2 = temp;
+    }
+
+    float invSlope1 = (v2.x - v1.x) / (v2.y - v1.y);
+    float invSlope2 = (v3.x - v1.x) / (v3.y - v1.y);
+
+    float curX1 = v1.x;
+    float curX2 = v1.x;
+
+    for (int scanlineY = v1.y; scanlineY <= v3.y; scanlineY++) {
+        for (int x = (int)curX1; x <= (int)curX2; x++) {
+            float t = (x - curX1) / (curX2 - curX1);
+            float depth = v1.z * (1.0 - t) + v2.z * t;
+            updateZBuffer(x, scanlineY, depth);
+        }
+        curX1 += invSlope1;
+        curX2 += invSlope2;
+    }
+}
+
+void printZBufferToFile(const char *filename) {
+    FILE *file = fopen(filename, "w");
+    if (!file) {
+        printf("Error opening file for writing.\n");
+        return;
+    }
+
+    for (int j = HEIGHT - 1; j >= 0; j--) {
+        for (int i = 0; i < WIDTH; i++) {
+            float normalizedDepth = 1.0 - (zBuffer[i][j].depth - INT_MAX) / (INT_MAX);
+            fprintf(file, "%.2f ", normalizedDepth);
+        }
+        fprintf(file, "\n");
+    }
+
+    fclose(file);
+}
+
+int main() {
+    initZBuffer();
+
+    Vertex v1 = {200, 100, 0.5};
+    Vertex v2 = {400, 500, 0.8};
+    Vertex v3 = {600, 200, 0.6};
+
+    renderTriangle(v1, v2, v3);
+
+    printZBufferToFile("zbuffer_output.txt");
+
+    return 0;
+}
+
